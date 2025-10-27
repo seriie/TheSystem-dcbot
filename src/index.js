27bc0c00bb300d587bac460fd83e4e492e5cc567
zee <mohammadzidane058@gmail.com>
2025-10-27 13:40:45 +0700

    feat: role checker for sendmsg command

--- src/index.js
+++ src/index.js
@@ -118,7 +118,14 @@ client.on("messageCreate", async (msg) => {
           msg.reply("Verification process started.");
           break;
         case "sendmsg":
-          myLogs(`${msg.author.displayName} trying to send dms`)
+          const allowedRoleId = process.env.DEVELOPER_ROLE_ID;
+
+          if (!msg.member.roles.cache.has(allowedRoleId)) {
+            await msg.reply("🚫 You are not allowed.");
+            return;
+          }
+
+          myLogs(`${msg.author.displayName} trying to send dms`);
           const [id, ...messageParts] = args.split(" ");
           const text = messageParts.join(" ");
 
@@ -132,12 +139,10 @@ client.on("messageCreate", async (msg) => {
             const user = await client.users.fetch(id);
             await user.send(text);
             msg.reply(`✅ Message sent to **${user.displayName}**`);
-            myLogs(`✅  Message sent to **${user.displayName}**`)
+            myLogs(`✅  Message sent to **${user.displayName}**`);
           } catch (err) {
             myLogs(err);
-            msg.reply(
-              "⚠️ Failed to send DM! recheck the ID!"
-            );
+            msg.reply("⚠️ Failed to send DM! recheck the ID!");
           }
           break;
         case "resetrank":
