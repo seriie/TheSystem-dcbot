aad88d323cec306a85bac9c4251a39a3b08cd82f
Zee <159999845+seriie@users.noreply.github.com>
2025-11-12 14:30:19 +0700

    update: limit get unranked users

--- src/messages/addRank.js
+++ src/messages/addRank.js
@@ -16,7 +16,7 @@ export const handleRankButton = async (interaction, client) => {
   if (interaction.customId !== "add_rank") return;
 
   await interaction.deferReply({ ephemeral: true }); // ⬅️ kasih waktu lebih
-  const users = await getUnrankedUsers(50);
+  const users = await getUnrankedUsers(25);
 
   if (!users.length) {
     return interaction.editReply({
